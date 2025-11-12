d20c78050b31ffaa17f54af27f18663e2efc2c92
Smetank0105 <206859408+Smetank0105@users.noreply.github.com>
2025-11-12 20:27:33 +0700

    Add project files.

---
+++ Server/main.cpp
@@ -0,0 +1,134 @@
+﻿#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif //!WIN32_LEAN_AND_MEAN
+
+#include<Windows.h>
+#include<WinSock2.h>
+#include<WS2tcpip.h>
+#include<iphlpapi.h>
+#include<iostream>
+using namespace std;
+
+#pragma comment(lib, "WS2_32.lib")
+
+#define DEFAULT_PORT	"27015"
+#define BUFFER_LENGHT	1460
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	DWORD dwLastError = 0;
+	INT iResult = 0;
+
+	//0)Инициализируем WinSock:
+	WSADATA wsaData;
+	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult != 0)
+	{
+		dwLastError = WSAGetLastError();
+		cout << "WSA init failed with: " << dwLastError << endl;
+		return dwLastError;
+	}
+
+	//1)Инициализируем переменные:
+	addrinfo* result = NULL;
+	addrinfo* ptr = NULL;
+	addrinfo hints;
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+	hints.ai_flags = AI_PASSIVE;
+
+	//2)Задаем параметры сокета:
+	iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
+	if (iResult != 0)
+	{
+		dwLastError = WSAGetLastError();
+		cout << "getaddrinfo failed with error: " << dwLastError << endl;
+		freeaddrinfo(result);
+		WSACleanup();
+		return dwLastError;
+	}
+
+	//3)Создаем сокет, который будет прослушивать Сервер:
+	SOCKET listen_socket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+	if (listen_socket == INVALID_SOCKET)
+	{
+		dwLastError = WSAGetLastError();
+		cout << "Socked creation failed with error: " << dwLastError << endl;
+		freeaddrinfo(result);
+		WSACleanup();
+		return dwLastError;
+	}
+
+	//4)Привязка сокета:
+	iResult = bind(listen_socket, result->ai_addr, result->ai_addrlen);
+	if (iResult == SOCKET_ERROR)
+	{
+		dwLastError = WSAGetLastError();
+		cout << "Bind failed with error: " << dwLastError << endl;
+		closesocket(listen_socket);
+		freeaddrinfo(result);
+		WSACleanup();
+		return dwLastError;
+	}
+
+	//5)Запускаем прослушивание сокета
+	iResult = listen(listen_socket, SOMAXCONN);
+	if (iResult == SOCKET_ERROR)
+	{
+		dwLastError = WSAGetLastError();
+		cout << "listen failed with error: " << dwLastError << endl;
+		closesocket(listen_socket);
+		freeaddrinfo(result);
+		WSACleanup();
+		return dwLastError;
+	}
+
+	//6)Обработка запросов от клиентов:
+	SOCKET client_socket = accept(listen_socket, NULL, NULL);
+	if (client_socket == INVALID_SOCKET)
+	{
+		dwLastError = WSAGetLastError();
+		cout << "Accept failed with error: " << dwLastError << endl;
+		closesocket(listen_socket);
+		freeaddrinfo(result);
+		WSACleanup();
+		return dwLastError;
+	}
+
+	//7)Получение запросов от клиента:
+	CHAR recv_buffer[BUFFER_LENGHT] = {};
+	CHAR send_buffer[BUFFER_LENGHT] = "Привет клиент";
+	INT iSendResult = 0;
+
+	do
+	{
+		iResult = recv(client_socket, recv_buffer, BUFFER_LENGHT, 0);
+		if (iResult > 0)
+		{
+			cout << iResult << " Bytes received, Message: " << recv_buffer << endl;
+			iSendResult = send(client_socket, send_buffer, sizeof(send_buffer), 0);
+			if (iSendResult == SOCKET_ERROR)
+			{
+				dwLastError = WSAGetLastError();
+				cout << "Send failed with error: " << dwLastError << endl;
+				break;
+			}
+			cout << "Bytes send: " << iSendResult << endl;
+		}
+		else if (iResult == 0) cout << "Connection closing" << endl;
+		else
+		{
+			dwLastError = WSAGetLastError();
+			cout << "Send failed with error: " << dwLastError << endl;
+			break;
+		}
+	} while (iResult > 0);
+	closesocket(client_socket);
+	closesocket(listen_socket);
+	freeaddrinfo(result);
+	WSACleanup();
+	return dwLastError;
+}
